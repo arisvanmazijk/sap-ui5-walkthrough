be85b52bae3780727e3f9734efcf357258f78b6c
arisvanmazijk <arisvanmazijk@hotmail.com>
2022-05-13 14:13:17 +0200

    Step 30: Routing and Navigation

--- webapp/Component.js
+++ webapp/Component.js
@@ -4,10 +4,10 @@ sap.ui.define([
 ], function (UIComponent, JSONModel) {
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-        metadata : {
+        metadata: {
             interfaces: ["sap.ui.core.IAsyncContentCreation"],
             manifest: "json"
-      },
+        },
         init: function () {
             // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
@@ -19,6 +19,9 @@ sap.ui.define([
             };
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
+
+            // create the views based on the url/hash
+            this.getRouter().initialize();
         }
     });
 });
