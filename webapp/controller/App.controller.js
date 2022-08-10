079c5cfb0e88272c2cd994ace9b8a297aa04dce4
arisvanmazijk <arisvanmazijk@hotmail.com>
2022-08-10 09:12:36 +0200

    Timeout verplaatst

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,105 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
-], function (Controller) {
+   "sap/ui/core/mvc/Controller",
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/core/Fragment"
+], function (Controller, JSONModel, Fragment) {
    "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
 
       onInit: function () {
          this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+
+         var oView = this.getView();
+
+         this.oModel = new JSONModel({
+            bIsExpiring: true,
+            iSecondsLeft: 99
+         });
+
+         oView.setModel(this.oModel);
+
+         this.setTimeOutDialog();
+      },
+
+      setTimeOutDialog: function () {
+         var self = this;
+         this.intervalHandle = setInterval(function () {
+            self.handleOpenDialog();
+         }, 15000);
+      },
+
+      handleOpenDialog: function () {
+         if (!this._oDialog) {
+            Fragment.load({
+               name: "sap.ui.demo.walkthrough.view.TimeoutDialog",
+               controller: this
+            }).then(function (oDialog) {
+               this._oDialog = oDialog;
+
+               this.getView().addDependent(this._oDialog);
+
+               this.onDialogOpen();
+            }.bind(this));
+         } else {
+            this.onDialogOpen();
+         }
+      },
+
+      onDialogOpen: function () {
+         this._oDialog.open();
+         this._startCounter();
+      },
+
+      onDialogClose: function () {
+         this._oDialog.close();
+         this._stopCounter();
+      },
+
+      onSignIn: function () {
+         this.onDialogClose();
+
+         // Insert your Sign In Logic here...
+
+      },
+
+      onExit: function () {
+         this._stopCounter();
+      },
+
+      _decrementCounter: function () {
+         this._onUpdateStatus();
+         if (this.iSeconds === 0) {
+            this._stopCounter();
+            this._onCounterEnd();
+            return;
+         }
+         this.iSeconds--;
+      },
+
+      _startCounter: function () {
+         this.iSeconds = this.getView().byId("expirationInput").getValue();
+         this._onCounterStart();
+         clearInterval(this.oTimer);
+         this.oTimer = setInterval(this._decrementCounter.bind(this), 1000);
+      },
+
+      _stopCounter: function () {
+         clearInterval(this.oTimer);
+         this.oTimer = null;
+      },
+
+      _onUpdateStatus: function () {
+         this.oModel.setProperty('/iSecondsLeft', this.iSeconds);
+      },
+
+      _onCounterStart: function () {
+         this.oModel.setProperty('/bIsExpiring', true);
+         this.oModel.setProperty('/iSecondsLeft', this.iSeconds);
+         this.iSeconds--;
+      },
+
+      _onCounterEnd: function () {
+         this.oModel.setProperty('/bIsExpiring', false);
       }
 
    });
--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -2,24 +2,10 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
 	"sap/ui/core/Fragment",
-	"sap/ui/core/syncStyleClass",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, Fragment, syncStyleClass, JSONModel) {
+	"sap/ui/core/syncStyleClass"
+], function (Controller, MessageToast, Fragment, syncStyleClass) {
 	"use strict";
 	return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
-
-		onInit: function () {
-			var oView = this.getView();
-
-			this.oModel = new JSONModel({
-				bIsExpiring: true,
-				iSecondsLeft: 99
-			});
-
-			oView.setModel(this.oModel);
-
-			this.setTimeOutDialog();
-		},
 
 		onShowHello: function () {
 			// read msg from i18n model
@@ -51,87 +37,7 @@ sap.ui.define([
 			// note: We don't need to chain to the pDialog promise, since this event-handler
 			// is only called from within the loaded dialog itself.
 			this.byId("helloDialog").close();
-		},
-
-		setTimeOutDialog: function() {
-			var self = this;
-			this.intervalHandle = setInterval(function() { 
-				self.handleOpenDialog();
-			 },  15000);
-		},
-
-		handleOpenDialog: function () {
-			if (!this._oDialog) {
-				Fragment.load({
-					name: "sap.ui.demo.walkthrough.view.TimeoutDialog",
-					controller: this
-				}).then(function (oDialog) {
-					this._oDialog = oDialog;
-
-					this.getView().addDependent(this._oDialog);
-
-					this.onDialogOpen();
-				}.bind(this));
-			} else {
-				this.onDialogOpen();
-			}
-		},
-
-		onDialogOpen: function () {
-			this._oDialog.open();
-			this._startCounter();
-		},
-
-		onDialogClose: function () {
-			this._oDialog.close();
-			this._stopCounter();
-		},
-
-		onSignIn: function () {
-			this.onDialogClose();
-
-			// Insert your Sign In Logic here...
-
-		},
-
-		onExit: function () {
-			this._stopCounter();
-		},
-
-		_decrementCounter: function () {
-			this._onUpdateStatus();
-			if (this.iSeconds === 0) {
-				this._stopCounter();
-				this._onCounterEnd();
-				return;
-			}
-			this.iSeconds--;
-		},
-
-		_startCounter: function () {
-			this.iSeconds = this.getView().byId("expirationInput").getValue();
-			this._onCounterStart();
-			clearInterval(this.oTimer);
-			this.oTimer = setInterval(this._decrementCounter.bind(this), 1000);
-		},
-
-		_stopCounter: function () {
-			clearInterval(this.oTimer);
-			this.oTimer = null;
-		},
-
-		_onUpdateStatus: function () {
-			this.oModel.setProperty('/iSecondsLeft', this.iSeconds);
-		},
-
-		_onCounterStart: function () {
-			this.oModel.setProperty('/bIsExpiring', true);
-			this.oModel.setProperty('/iSecondsLeft', this.iSeconds);
-			this.iSeconds--;
-		},
-
-		_onCounterEnd: function () {
-			this.oModel.setProperty('/bIsExpiring', false);
 		}
+
 	});
 });
